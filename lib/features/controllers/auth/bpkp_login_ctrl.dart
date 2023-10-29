4f605f7e856aca107ece60c6d05c1f45702b4b18
unknown <nanda.sitepu@gmail.com>
2023-10-29 14:06:25 +0700

    asasdad

---
+++ lib/features/controllers/auth/bpkp_login_ctrl.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BPKPLogin extends StatelessWidget {
+  const BPKPLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/controllers/sharedpref.dart
@@ -0,0 +1,44 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
+
+class SharedPrefrence {
+  Future<bool> setToken(String token) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString("token", token);
+  }
+
+  Future<String> getToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString("token") ?? '';
+  }
+
+  Future<bool> setUserId(String userId) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString("user_id", userId);
+  }
+
+  Future<String> getUserId() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString("user_id") ?? '';
+  }
+
+  Future<bool> setName(String name) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString("name", name);
+  }
+
+  Future<String> getName() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString("name") ?? '';
+  }
+
+  Future<bool> setLoggedIn(bool status) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setBool("logged_in", status);
+  }
+
+  Future<bool> getLogedIn() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool("logged_in") ?? false;
+  }
+}
